3cc5aaf9c79a377760d68b8151f2031807b2ee89
Paulo Bruny Lima <paulobruny@gmail.com>
2021-02-24 16:31:41 -0300

    add styles to homepage with css module

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import CompletedChallenge from '../components/CompletedChallenges'
+import ExperienceBar from '../components/ExperienceBar'
+import Profile from '../components/Profile'
+
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenge />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  )
+}
