1cbccc385d42dc3aff7283ec49bc5f1de3f9d9b1
Paulo Bruny Lima <paulobruny@gmail.com>
2021-02-24 16:43:27 -0300

    add countdown, title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import Head from 'next/head'
+
 import CompletedChallenge from '../components/CompletedChallenges'
+import Countdown from '../components/Countdown'
 import ExperienceBar from '../components/ExperienceBar'
 import Profile from '../components/Profile'
 
@@ -7,12 +10,16 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home - Mova-se</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenge />
+          <Countdown />
         </div>
         <div></div>
       </section>
