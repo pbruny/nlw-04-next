f8c7c61be258fbef80cac926e2cf00d2ce464210
Paulo Bruny Lima <paulobruny@gmail.com>
2021-02-25 18:30:01 -0300

    add meta description tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Se liga aí é uma aplicação gamificada para ajudar pessoas a se exercitarem enquanto cumprem sessões de foco."
           />
         </Head>
         <body>
