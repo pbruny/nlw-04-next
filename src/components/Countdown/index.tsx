4ddd36159e51bd820aa78466bab0b4312398efdd
Paulo Bruny Lima <paulobruny@gmail.com>
2021-02-24 22:18:31 -0300

    finish countdown component

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import styles from '../../styles/components/Countdown.module.css'
 
+let countdownTimeout: NodeJS.Timeout
+
 export default function Countdown() {
-  const [time, setTime] = useState(2 * 60)
+  const [time, setTime] = useState(0.05 * 60)
   const [isActive, setIsActive] = useState(false)
+  const [hasFinished, setHasFinished] = useState(false)
 
   const minutes = Math.floor(time / 60)
   const seconds = time % 60
@@ -13,14 +16,23 @@ export default function Countdown() {
 
   useEffect(() => {
     if (isActive && time > 0) {
-      setTimeout(() => {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1)
       }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true)
+      setIsActive(false)
     }
   }, [isActive, time])
 
   function startCountdown() {
-    setIsActive(!isActive)
+    setIsActive(true)
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout)
+    setIsActive(false)
+    setTime(25 * 60)
   }
 
   return (
@@ -37,23 +49,35 @@ export default function Countdown() {
         </div>
       </div>
 
-      {isActive ? (
-        <button
-          type="button"
-          className={styles.countdownButton + ' ' + styles.countdownCloseButton}
-          onClick={startCountdown}
-        >
-          Abandonar Ciclo{' '}
-          <img src="icons/close.svg" alt="Símbolo de um x para fechar" />
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          Ciclo finalizado{' '}
+          <img
+            src="icons/check_circle.svg"
+            alt="Símbolo de uma seta para cima"
+          />
         </button>
       ) : (
-        <button
-          type="button"
-          className={styles.countdownButton}
-          onClick={startCountdown}
-        >
-          Iniciar um ciclo
-        </button>
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownCloseButton} ${styles.countdownButton}`}
+              onClick={resetCountdown}
+            >
+              Abandonar Ciclo{' '}
+              <img src="icons/close.svg" alt="Símbolo de um x para fechar" />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
       )}
     </div>
   )
