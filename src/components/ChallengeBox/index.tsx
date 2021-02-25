49c1fc8359388cc00e7efbb9f56c75b982b3b431
Paulo Bruny Lima <paulobruny@gmail.com>
2021-02-25 16:55:16 -0300

    set challenge box to be dynamic

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,28 +1,44 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../../contexts/ChallengeContext'
 import styles from '../../styles/components/ChallengeBox.module.css'
 
 export default function ChallengeBox() {
-  const hasActiveChallenge = true
+  const { activeChallenge, completeChallenge, resetChallenge } = useContext(
+    ChallengesContext
+  )
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeBoxActive}>
-          <header>Ganhe 400 XP</header>
+          <header>Ganhe {activeChallenge.amount} XP</header>
 
           <main>
             <img
-              src="icons/body.svg"
+              src={
+                activeChallenge.type === 'body'
+                  ? 'icons/body.svg'
+                  : 'icons/eye.svg'
+              }
               alt="Imagem de uma mão com uma anilha de pesos"
             />
             <strong>Novo desafio</strong>
-            <p>Faça um alongamento de 1 minuto após levantar-se</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
-            <button type="button" className={styles.challengeFailedButton}>
+            <button
+              onClick={resetChallenge}
+              type="button"
+              className={styles.challengeFailedButton}
+            >
               Falhei
             </button>
-            <button type="button" className={styles.challengeSucceededButton}>
+            <button
+              onClick={() => completeChallenge(activeChallenge.amount)}
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
               Completei
             </button>
           </footer>
